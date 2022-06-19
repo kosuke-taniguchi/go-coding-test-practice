6bd10c0983b04c5acfbbf80cf9c0c9454e70203a
KosukeTaniguchi <applekkttegg@yahoo.co.jp>
2022-06-19 15:57:45 +0900

    lowestCommonAncestor

--- main.go
+++ main.go
@@ -2069,6 +2069,20 @@ func isPalindromeListNodeV2(head *ListNode) bool {
 	return true
 }
 
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+
+	if root.Val > p.Val && root.Val > q.Val {
+		return lowestCommonAncestor(root.Left, p, q)
+	} else if root.Val < p.Val && root.Val < q.Val {
+		return lowestCommonAncestor(root.Right, p, q)
+	} else {
+		return root
+	}
+}
+
 func main() {
 	//numbers := []int{0, 2, 3, 4, 6, 8, 9}
 	//fmt.Println(numbers)
